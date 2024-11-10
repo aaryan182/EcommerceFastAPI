ecc0fe1d709c5f4c32e7da08f67c99222e9ad092
aaryan <aaryanbajaj385@gmail.com>
2024-11-10 09:41:23 +0530

    feat: added configuration file

---
+++ app/config/settings.py
@@ -0,0 +1,26 @@
+from pydantic import BaseSettings
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    #Database_url
+    DATABASE_URL: str = "postgresql://user:password@localhost:5432/ecomm"
+    
+    #JWT settings
+    SECRET_KEY: str = "aaryan"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    
+    #API settings
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "E-commerce API"
+    
+    class Config: 
+        case_sensitive = True
+        env_file = ".env"
+    
+@lru_cache()
+def get_settings():
+    # using lru_cache to prevent multiple reads of .env file
+    return Settings()
+
+settings = get_settings()
